5f6655ebf5cdd643147d32bf2b75a3c041099bda
Douglas Sangster <d.sangster@hotmail.co.uk>
2017-12-14 12:21:26 +0000

    created methods in House class for finding house with id and finding all houses from database

--- models/house.rb
+++ models/house.rb
@@ -8,6 +8,7 @@ def initialize ( options )
   @logo = options['logo']
 end
 
+
 def save
   sql = "INSERT INTO houses
         (name, logo)
@@ -17,5 +18,27 @@ def save
   id_hash = SqlRunner.run(sql, values).first
   @id = id_hash['id'].to_i
 end
+
+
+def House.find_all()
+  sql = "SELECT * FROM houses;"
+  house_hashes = SqlRunner.run(sql)
+  houses_array = house_hashes.map do |house_hash|
+    House.new(house_hash)
+  end
+  return houses_array
+end
+
+
+def self.find(id)
+  sql = "
+        SELECT * FROM houses
+        WHERE id = $1;
+        "
+  house_hash = SqlRunner.run(sql, [id]).first
+  house = House.new(house_hash)
+  return house
+end
+
 
 end
--- runner_file.rb
+++ runner_file.rb
@@ -1,9 +1,15 @@
-require_relative('models/student')
 require('pry-byebug')
+require_relative('models/student')
+require_relative('models/house')
+
 
 student1 = Student.find(1)
 student3 = Student.find(3)
 students = Student.find_all
+
+house2 = House.find(2)
+house4 = House.find(4)
+houses = House.find_all
 
 binding.pry
 
